81000f2fb91254bfad5e751b302248147476161b
cylefebvre <cyril.lefebvre@soprasteria.com>
2017-10-09 18:30:31 +0200

    feat(refonte welcome)

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { MainBarService } from '../shared/main-bar/main-bar.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-    selector: 'app-welcome',
-    templateUrl: './welcome.component.html',
-    styleUrls: ['./welcome.component.scss'],
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit {
   constructor(
-    private mainBarService: MainBarService
+    private mainBarService: MainBarService,
+    private route: ActivatedRoute,
   ) {}
 
   ngOnInit(): void {
